ef945a9122392366fcee30aa5fd0de5f358cc0ee
wzshiming <wzshiming@foxmail.com>
2018-07-16 16:01:32 +0800

    cleanup

--- winseq_windows.go
+++ winseq_windows.go
@@ -7,21 +7,29 @@ import (
 	"unsafe"
 )
 
+// This package only works with Windows.
+// Virtual terminal sequences are control character sequences that can control cursor movement,
+// color/font mode, and other operations when written to the output stream.
+// Sequences may also be received on the input stream in response to an output stream query information sequence or
+// as an encoding of user input when the appropriate mode is set.
+
 func init() {
-	applyUnixlikeMode()
+	enableVirtualTerminalProcessing()
 }
 
 var (
-	kernel32                        = syscall.NewLazyDLL("kernel32.dll")
-	procGetConsoleMode              = kernel32.NewProc("GetConsoleMode")
-	procSetConsoleMode              = kernel32.NewProc("SetConsoleMode")
-	hStdout                         = uintptr(syscall.Stdout)
-	enableVirtualTerminalProcessing = uintptr(0x0004)
+	kernel32                      = syscall.NewLazyDLL("kernel32.dll")
+	procGetConsoleMode            = kernel32.NewProc("GetConsoleMode")
+	procSetConsoleMode            = kernel32.NewProc("SetConsoleMode")
+	hStdout                       = uintptr(syscall.Stdout)
+	flagVirtualTerminalProcessing = uintptr(0x0004)
 )
 
-func applyUnixlikeMode() error {
+func enableVirtualTerminalProcessing() (err error) {
 	var mode uintptr
-	ok, _, err := procGetConsoleMode.Call(hStdout, uintptr(unsafe.Pointer(&mode)))
+	var ok uintptr
+	defer recover()
+	ok, _, err = procGetConsoleMode.Call(hStdout, uintptr(unsafe.Pointer(&mode)))
 	if ok != 0 {
 		err = nil
 	}
@@ -29,13 +37,17 @@ func applyUnixlikeMode() error {
 		return err
 	}
 
-	if mode&enableVirtualTerminalProcessing == mode|enableVirtualTerminalProcessing {
+	if 0 != mode&flagVirtualTerminalProcessing {
 		return nil
 	}
 
-	ok, _, err = procSetConsoleMode.Call(hStdout, mode|enableVirtualTerminalProcessing)
+	ok, _, err = procSetConsoleMode.Call(hStdout, mode|flagVirtualTerminalProcessing)
 	if ok != 0 {
 		err = nil
 	}
-	return err
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
